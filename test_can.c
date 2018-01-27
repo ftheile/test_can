516d1e8390bf9e15a0367bb219fba4fdd7bc8ca5
Frank Theile <frank.theile@web.de>
2018-01-28 00:54:10 +0100

    Pass CAN interface on the command line

--- test_can.c
+++ test_can.c
@@ -9,38 +9,42 @@
 
 int main(int argc, char* argv[])
 {
-	int s;
+	if (argc == 2) {
+		int s;
 
-	s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
-	if (s != -1) {
-		struct sockaddr_can addr;
-		addr.can_family = AF_CAN;
-		addr.can_ifindex = if_nametoindex("can0");
-		if (addr.can_ifindex != 0) {
-			if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) == 0) {
-				int n;
-				for (n=0; n<10; n++) {
-					struct can_frame frame;
-					if (read(s, &frame, sizeof(frame)) == sizeof(frame)) {
-						int i;
-						printf("0x%X %d ", frame.can_id & CAN_EFF_MASK, frame.can_dlc);
-						for (i=0; i<frame.can_dlc; i++) {
-							printf("0x%.2X ");
+		s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+		if (s != -1) {
+			struct sockaddr_can addr;
+			addr.can_family = AF_CAN;
+			addr.can_ifindex = if_nametoindex(argv[1]);
+			if (addr.can_ifindex != 0) {
+				if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) == 0) {
+					int n;
+					for (n=0; n<10; n++) {
+						struct can_frame frame;
+						if (read(s, &frame, sizeof(frame)) == sizeof(frame)) {
+							int i;
+							printf("0x%X %d ", frame.can_id & CAN_EFF_MASK, frame.can_dlc);
+							for (i=0; i<frame.can_dlc; i++) {
+								printf("0x%.2X ");
+							}
+							printf("\n");
+						} else {
+							perror("read() error");
 						}
-						printf("\n");
-					} else {
-						perror("read() error");
 					}
+				} else {
+					perror("bind() error");
 				}
 			} else {
-				perror("bind() error");
+				perror("if_nametoindex() error");
 			}
 		} else {
-			perror("if_nametoindex() error");
+			perror("socket() error");
 		}
 	} else {
-		perror("socket() error");
+		printf("Help:\n");
+		printf("  %s <interface>\n", argv[0]);
 	}
-
 	return 0;
 }
